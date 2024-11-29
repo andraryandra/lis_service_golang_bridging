1e8ba8733b28faa7476e7a6719b879f8d73ff978
andraryandra <andraryandra38@gmail.com>
2024-11-29 15:04:20 +0700

    feat: :hammer: add nullable payload, and fix icd10

--- main.go
+++ main.go
@@ -1,155 +1,174 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
-	"os"
+    "bytes"
+    "encoding/json"
+    "fmt"
+    "io"
+    "log"
+    "net/http"
+    "os"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
-	"github.com/joho/godotenv"
+    "github.com/go-chi/chi/v5"
+    "github.com/go-chi/chi/v5/middleware"
+    "github.com/go-playground/validator/v10"
+    "github.com/joho/godotenv"
 )
 
-type SimrsRequest struct {
-	NoPendaftaran string `json:"no_pendaftaran"`
-	NoRM          string `json:"no_rm"`
-	NoOrder       string `json:"no_order"`
-	NamaPasien    string `json:"nama_pasien"`
-	TempatLahir   string `json:"tempat_lahir"`
-	TglLahir      string `json:"tgl_lahir"`
-	JK            string `json:"jk"`
-	Alamat        string `json:"alamat"`
-	Phone         string `json:"phone,omitempty"`
-	NIK           string `json:"nik"`
-	IdJenisPasien string `json:"id_jenis_pasien"`
-	JenisPasien   string `json:"jenis_pasien,omitempty"`
-	IdPenjamin    string `json:"id_penjamin"`
-	Penjamin      string `json:"penjamin,omitempty"`
-	RujukanAsal   string `json:"rujukan_asal"`
-	DetailRujukan []struct {
-		IdDokter    string `json:"id_dokter"`
-		NamaDokter  string `json:"nama_dokter"`
-		IdWard      string `json:"id_ward"`
-		Ward        string `json:"ward"`
-		IdFasilitas string `json:"id_fasilitas"`
-		Fasilitas   string `json:"fasilitas"`
-	} `json:"detail_rujukan"`
-	Cito              string   `json:"cito"`
-	Diagnose          string   `json:"diagnose"`
-	RegistrationDate  string   `json:"registration_date"`
-	Email             string   `json:"email"`
-	Region            string   `json:"region"`
-	CodeClinic        string   `json:"code_clinic"`
-	ClinicName        string   `json:"clinic_name"`
-	OrderNote         string   `json:"order_note"`
-	KategoriKunjungan string   `json:"kategori_kunjungan"`
-	ICD10             []string `json:"icd10"`
-	Order             []struct {
-		IdTest   string `json:"id_test"`
-		NamaTest string `json:"nama_test"`
-	} `json:"order"`
+// type ICD10 struct {
+//     Type string `json:"type,omitempty"`
+//     Code string `json:"code,omitempty"`
+//     Text string `json:"text,omitempty"`
+// }
+
+type DetailRujukan struct {
+    IdDokter       *string `json:"id_dokter" validate:"required"`
+    NamaDokter     *string `json:"nama_dokter" validate:"required"`
+    PhoneDokter    *string `json:"phone_dokter,omitempty"`
+    IhsDokter      *string `json:"ihs_dokter,omitempty"`
+    Nik            *string `json:"nik,omitempty"`
+    IdWard         *string `json:"id_ward,omitempty"`
+    Ward           *string `json:"ward,omitempty"`
+    IdFasilitas    *string `json:"id_fasilitas,omitempty"`
+    Fasilitas      *string `json:"fasilitas,omitempty"`
+    Alamat         *string `json:"alamat,omitempty"`
+    NoTelpPerujuk  *string `json:"no_telp_perujuk,omitempty"`
+    IdInstalasi    *string `json:"id_instalasi,omitempty"`
 }
 
+type Order struct {
+    IdTest   *string `json:"id_test" validate:"required"`
+    NamaTest *string `json:"nama_test,omitempty"`
+}
+
+type SimrsRequest struct {
+    NoPendaftaran     *string         `json:"no_pendaftaran" validate:"required"`
+    NoRM              *string         `json:"no_rm" validate:"required"`
+    NoOrder           *string         `json:"no_order" validate:"required"`
+    NamaPasien        *string         `json:"nama_pasien" validate:"required"`
+    TempatLahir       *string         `json:"tempat_lahir"`
+    TglLahir          *string         `json:"tgl_lahir,omitempty"`
+    JK                *string         `json:"jk" validate:"required"`
+    Alamat            *string         `json:"alamat,omitempty"`
+    Phone             *string         `json:"phone,omitempty"`
+    NIK               *string         `json:"nik,omitempty"`
+    IdJenisPasien     *string         `json:"id_jenis_pasien,omitempty"`
+    JenisPasien       *string         `json:"jenis_pasien,omitempty"`
+    IdPenjamin        *string         `json:"id_penjamin,omitempty"`
+    Penjamin          *string         `json:"penjamin,omitempty"`
+    RujukanAsal       *string         `json:"rujukan_asal" validate:"required"`
+    DetailRujukan     []*DetailRujukan `json:"detail_rujukan" validate:"required,dive"`
+    Cito              *string         `json:"cito" validate:"required"`
+    Diagnose          *string         `json:"diagnose,omitempty"`
+    RegistrationDate  *string         `json:"registration_date,omitempty"`
+    Email             *string         `json:"email,omitempty"`
+    Region            *string         `json:"region,omitempty"`
+    CodeClinic        *string         `json:"code_clinic,omitempty"`
+    ClinicName        *string         `json:"clinic_name,omitempty"`
+    OrderNote         *string         `json:"order_note,omitempty"`
+    KategoriKunjungan *string         `json:"kategori_kunjungan,omitempty"`
+    ICD10             *interface{}        `json:"icd10,omitempty"`
+    Order             []*Order        `json:"order" validate:"required,dive"`
+}
+
+var validate *validator.Validate
+
 type LISRequest struct {
-	NoPendaftaran string `json:"no_pendaftaran"`
-	NoRM          string `json:"no_rm"`
-	NoOrder       string `json:"no_order"`
-	NamaPasien    string `json:"nama_pasien"`
-	TempatLahir   string `json:"tempat_lahir"`
-	TglLahir      string `json:"tgl_lahir"`
-	JK            string `json:"jk"`
-	Alamat        string `json:"alamat"`
-	Phone         string `json:"phone,omitempty"`
-	NIK           string `json:"nik"`
-	IdJenisPasien string `json:"id_jenis_pasien"`
-	JenisPasien   string `json:"jenis_pasien,omitempty"`
-	IdPenjamin    string `json:"id_penjamin"`
-	Penjamin      string `json:"penjamin,omitempty"`
-	RujukanAsal   string `json:"rujukan_asal"`
-	DetailRujukan []struct {
-		IdDokter    string `json:"id_dokter"`
-		NamaDokter  string `json:"nama_dokter"`
-		IdWard      string `json:"id_ward"`
-		Ward        string `json:"ward"`
-		IdFasilitas string `json:"id_fasilitas"`
-		Fasilitas   string `json:"fasilitas"`
-	} `json:"detail_rujukan"`
-	Cito              bool     `json:"cito"`
-	Diagnose          string   `json:"diagnose"`
-	Email             string   `json:"email"`
-	Region            string   `json:"region"`
-	CodeClinic        string   `json:"code_clinic"`
-	ClinicName        string   `json:"clinic_name"`
-	OrderNote         string   `json:"order_note"`
-	KategoriKunjungan string   `json:"kategori_kunjungan"`
-	ICD10             []string `json:"icd10"`
-	Order             []struct {
-		IdTest   string `json:"id_test"`
-		NamaTest string `json:"nama_test"`
-	} `json:"order"`
+    NoPendaftaran     *string         `json:"no_pendaftaran"`
+    NoRM              *string         `json:"no_rm"`
+    NoOrder           *string         `json:"no_order"`
+    NamaPasien        *string         `json:"nama_pasien"`
+    TempatLahir       *string         `json:"tempat_lahir"`
+    TglLahir          *string         `json:"tgl_lahir"`
+    JK                *string         `json:"jk"`
+    Alamat            *string         `json:"alamat"`
+    Phone             *string         `json:"phone,omitempty"`
+    NIK               *string         `json:"nik"`
+    IdJenisPasien     *string         `json:"id_jenis_pasien"`
+    JenisPasien       *string         `json:"jenis_pasien,omitempty"`
+    IdPenjamin        *string         `json:"id_penjamin"`
+    Penjamin          *string         `json:"penjamin,omitempty"`
+    RujukanAsal       *string         `json:"rujukan_asal"`
+    DetailRujukan     []*DetailRujukan `json:"detail_rujukan"`
+    Cito              *bool           `json:"cito"`
+    Diagnose          *string         `json:"diagnose,omitempty"`
+    RegistrationDate  *string         `json:"registration_date,omitempty"`
+    Email             *string         `json:"email,omitempty"`
+    Region            *string         `json:"region,omitempty"`
+    CodeClinic        *string         `json:"code_clinic,omitempty"`
+    ClinicName        *string         `json:"clinic_name,omitempty"`
+    OrderNote         *string         `json:"order_note,omitempty"`
+    KategoriKunjungan *string         `json:"kategori_kunjungan,omitempty"`
+	ICD10			  *interface{}     `json:"icd10"`
+    Order             []*Order        `json:"order"`
 }
 
 func transformToLIS(simrs *SimrsRequest) *LISRequest {
-	var idJenisPasien, jenisPasien, idPenjamin, penjamin string
+    var idJenisPasien, jenisPasien, idPenjamin, penjamin string
 
-	if simrs.IdJenisPasien == "UMUM" {
-		idJenisPasien = "1"
-		jenisPasien = "UMUM"
-		idPenjamin = "UMUM"
-		penjamin = " "
-	} else {
-		idJenisPasien = "2"
-		jenisPasien = "ASURANSI"
-		idPenjamin = simrs.IdJenisPasien
-		penjamin = simrs.JenisPasien
-	}
+    if simrs.IdJenisPasien == nil || *simrs.IdJenisPasien == "UMUM" {
+        idJenisPasien = "1"
+        jenisPasien = "UMUM"
+        idPenjamin = "UMUM"
+        penjamin = " "
+    } else {
+        idJenisPasien = "2"
+        jenisPasien = "ASURANSI"
+        idPenjamin = *simrs.IdJenisPasien
+        penjamin = *simrs.JenisPasien
+    }
 
-	// Transform Cito to bool
-	var cito bool
-	if simrs.Cito == "1" || simrs.Cito == "true" {
-		cito = true
-	} else {
-		cito = false
-	}
+    // Transform Cito to bool
+    var cito bool
+    if simrs.Cito != nil && (*simrs.Cito == "1" || *simrs.Cito == "true") {
+        cito = true
+    } else {
+        cito = false
+    }
 
-	// Map SimrsRequest to LISRequest
-	return &LISRequest{
-		NoPendaftaran:     simrs.NoPendaftaran,
-		NoRM:              simrs.NoRM,
-		NoOrder:           simrs.NoOrder,
-		NamaPasien:        simrs.NamaPasien,
-		TempatLahir:       simrs.TempatLahir,
-		TglLahir:          simrs.TglLahir,
-		JK:                simrs.JK,
-		Alamat:            simrs.Alamat,
-		Phone:             simrs.Phone,
-		NIK:               simrs.NIK,
-		IdJenisPasien:     idJenisPasien,
-		JenisPasien:       jenisPasien,
-		IdPenjamin:        idPenjamin,
-		Penjamin:          penjamin,
-		RujukanAsal:       simrs.RujukanAsal,
-		DetailRujukan:     simrs.DetailRujukan,
-		Cito:              cito,
-		Diagnose:          simrs.Diagnose,
-		Email:             simrs.Email,
-		Region:            simrs.Region,
-		CodeClinic:        simrs.CodeClinic,
-		ClinicName:        simrs.ClinicName,
-		OrderNote:         simrs.OrderNote,
-		KategoriKunjungan: simrs.KategoriKunjungan,
-		ICD10:             simrs.ICD10,
-		Order:             simrs.Order,
-	}
+    var icd10 interface{}
+    if simrs.ICD10 != nil {
+        icd10 = simrs.ICD10
+    } else {
+        icd10 = []interface{}{}
+    }
+
+    // Map SimrsRequest to LISRequest
+    return &LISRequest{
+        NoPendaftaran:     simrs.NoPendaftaran,
+        NoRM:              simrs.NoRM,
+        NoOrder:           simrs.NoOrder,
+        NamaPasien:        simrs.NamaPasien,
+        TempatLahir:       simrs.TempatLahir,
+        TglLahir:          simrs.TglLahir,
+        JK:                simrs.JK,
+        Alamat:            simrs.Alamat,
+        Phone:             simrs.Phone,
+        NIK:               simrs.NIK,
+        IdJenisPasien:     &idJenisPasien,
+        JenisPasien:       &jenisPasien,
+        IdPenjamin:        &idPenjamin,
+        Penjamin:          &penjamin,
+        RujukanAsal:       simrs.RujukanAsal,
+        DetailRujukan:     simrs.DetailRujukan,
+        Cito:              &cito,
+        Diagnose:          simrs.Diagnose,
+        RegistrationDate:  simrs.RegistrationDate,
+        Email:             simrs.Email,
+        Region:            simrs.Region,
+        CodeClinic:        simrs.CodeClinic,
+        ClinicName:        simrs.ClinicName,
+        OrderNote:         simrs.OrderNote,
+        KategoriKunjungan: simrs.KategoriKunjungan,
+        ICD10:             &icd10,
+        Order:             simrs.Order,
+    }
 }
 
 type SendToLisBridgingIn struct {
-	request *LISRequest
-	xSign   string
-	xCons   string
+    request *LISRequest
+    xSign   string
+    xCons   string
 }
 
 func sendToLISBridging(payload SendToLisBridgingIn) (map[string]interface{}, error) {
@@ -195,63 +214,82 @@ func sendToLISBridging(payload SendToLisBridgingIn) (map[string]interface{}, err
 }
 
 func handleRequest(w http.ResponseWriter, r *http.Request) {
-	var simrsReq SimrsRequest
+    var simrsReq SimrsRequest
 
-	err := json.NewDecoder(r.Body).Decode(&simrsReq)
+    // Log the incoming request body
+    body, err := io.ReadAll(r.Body)
+    if err != nil {
+        http.Error(w, "Failed to read request body", http.StatusBadRequest)
+        return
+    }
+    log.Printf("Incoming request body: %s", body)
 
-	if err != nil {
-		http.Error(w, "Invalid request payload", http.StatusBadRequest)
-		return
-	}
+    // Decode the request body into the SimrsRequest struct
+    err = json.Unmarshal(body, &simrsReq)
+    if err != nil {
+        http.Error(w, "Invalid request payload", http.StatusBadRequest)
+        log.Printf("Invalid request payload: %v", err)
+        return
+    }
 
-	// Transform SIMRS DTO to LIS DTO
-	lisReq := transformToLIS(&simrsReq)
+    // Validate the request payload
+    err = validate.Struct(simrsReq)
+    if err != nil {
+        validationErrors := err.(validator.ValidationErrors)
+        http.Error(w, fmt.Sprintf("Validation error: %v", validationErrors), http.StatusBadRequest)
+        log.Printf("Validation error: %v", validationErrors)
+        return
+    }
 
-	// Send the transformed data to LIS bridging
-	payload := SendToLisBridgingIn{
-		request: lisReq,
-		xSign:   r.Header.Get("x-sign"),
-		xCons:   r.Header.Get("x-cons"),
-	}
+    // Transform SIMRS DTO to LIS DTO
+    lisReq := transformToLIS(&simrsReq)
 
-	respBody, err := sendToLISBridging(payload)
+    // Send the transformed data to LIS bridging
+    payload := SendToLisBridgingIn{
+        request: lisReq,
+        xSign:   r.Header.Get("x-sign"),
+        xCons:   r.Header.Get("x-cons"),
+    }
 
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Error sending data to LIS bridging: %v", err), http.StatusInternalServerError)
-		log.Printf("Error sending data to LIS bridging: %v", err)
-		return
-	}
+    respBody, err := sendToLISBridging(payload)
+    if err != nil {
+        http.Error(w, fmt.Sprintf("Error sending data to LIS bridging: %v", err), http.StatusInternalServerError)
+        log.Printf("Error sending data to LIS bridging: %v", err)
+        return
+    }
 
-	// Respond with the transformed data
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(respBody)
-	log.Printf("\n------------------------------------------------------------------------------------------------------------------\n")
+    // Respond with the transformed data
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(respBody)
+    log.Printf("\n------------------------------------------------------------------------------------------------------------------\n")
 
-	// Log the response status
-	log.Printf("Response status:\n %d", http.StatusOK)
+    // Log the response status
+    log.Printf("Response status:\n %d", http.StatusOK)
 }
 
 func main() {
-	// Load environment variables from .env file
-	err := godotenv.Load()
+    // Initialize the validator
+    validate = validator.New()
 
-	if err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
-	}
+    // Load environment variables from .env file
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatalf("Error loading .env file: %v", err)
+    }
 
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World!"))
-	})
+    r := chi.NewRouter()
+    r.Use(middleware.Logger)
+    r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+        w.Write([]byte("Hello World!"))
+    })
 
-	r.Post("/api/v1/saveOrder", handleRequest)
+    r.Post("/api/v1/saveOrder", handleRequest)
 
-	httpServer := &http.Server{
-		Addr:    ":8111",
-		Handler: r,
-	}
+    httpServer := &http.Server{
+        Addr:    ":8111",
+        Handler: r,
+    }
 
-	log.Printf("Starting server on port %s\n", "8111")
-	log.Fatal(httpServer.ListenAndServe())
+    log.Printf("Starting server on port %s\n", "8111")
+    log.Fatal(httpServer.ListenAndServe())
 }
